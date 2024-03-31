4816bc653cb16e563dc90ef1417590389e376d81
pcardenal <pcardenalra@gmail.com>
2024-03-31 17:48:31 +0200

    Add List and it's display

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,31 @@ impl fmt::Display for Complex {
 }
 
 
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		// Extract the value using tuple indexing, and create a reference to vec
+		let vec = &self.0;
+		
+		write!(f, "[")?;  
+		// Iterate over `v` in `vec` while enumerating the iteration
+		// count in `count`.
+		for (count, v) in vec.iter().enumerate() {
+			// For every element except the first, add a comma.
+			// Use the ? operator to return on errors.
+			if count != 0 {
+				write!(f, ", ")?;
+			}
+			write!(f, "{}", v)?;
+		}
+		  
+		// Close the opened bracket and return a fmt::Result value.
+		write!(f, "]")
+	}
+}
+
 fn main() {
 
     let x : i16 = 5 + 90 + 5;
@@ -123,5 +148,8 @@ Como ves, se pueden usar argumentos posicionales o con nombres x_2 = {1} and x_1
     println!("Compare points:");
     println!("Display: {}", z);
     println!("Debug: {:?}", z);
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
 
 }
