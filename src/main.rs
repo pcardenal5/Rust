7d10b907e85aab6f3daa76aa35aa1d4fcb6e77ac
pcardenal <pcardenalra@gmail.com>
2024-03-31 18:04:52 +0200

    Create City and color, implement Display

--- src/main.rs
+++ src/main.rs
@@ -81,6 +81,47 @@ impl fmt::Display for List {
 	}
 }
 
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+
+impl fmt::Display for City {
+    // `f` is a buffer, and this method must write the formatted string into it.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` is like `format!`, but it will write the formatted string
+        // into a buffer (the first argument).
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+// Queremos que salga así: RGB (128, 255, 90) 0x80FF5A
+impl fmt::Display for Color{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(
+            f, 
+            "RGB ({r} {g} {b}) 0x{r:0>2X}{g:0>2X}{b:0>2X}", 
+            r = self.red,
+            g = self.green,
+            b = self.blue
+        )
+    }
+}
+
 fn main() {
 
     let x : i16 = 5 + 90 + 5;
@@ -152,4 +193,21 @@ Como ves, se pueden usar argumentos posicionales o con nombres x_2 = {1} and x_1
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
 
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ] {
+        println!("{}", city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ] {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{}", color);
+    }
+    
 }
