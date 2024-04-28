ead2f64f6a1cd2b0d749c5a63643b46bc0a4c360
pcardenal <pcardenalra@gmail.com>
2024-04-28 22:00:44 +0200

    Add terrible fibonacci

--- flow/src/main.rs
+++ flow/src/main.rs
@@ -9,6 +9,27 @@ use std::io;
 
 fn main() {
     
+    println!("Escoja la aplicación:");
+    println!("\t1: Coversor de Farenheit a Celsius");
+    println!("\t2: Obtener el n-ésimo número de Fibonacci");
+
+    let mut choice = String::new();
+
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("No se ha podido leer la línea");
+
+    let choice = choice.trim();
+
+    if choice == "1"{
+        farenheitHandler();
+    } else if choice == "2"{
+        fibonacciHandler();
+    }
+}
+
+fn farenheitHandler(){
+
     println!("Introduzca una temperatura en Farenheit:");
 
     let mut fDegrees = String::new();
@@ -19,10 +40,36 @@ fn main() {
     
     let fDegrees: f64 =  fDegrees.trim().parse().expect("Por favor, introduzca un número");
 
-    let fCelsius: f64  = fahrenheitToCelsius(fDegrees);
+    let fCelsius: f64  = farenheitToCelsius(fDegrees);
     println!("Ha introducido {fDegrees}ºF, que es equivalente a {fCelsius}ºC");
 }
 
-fn fahrenheitToCelsius(fDegrees:f64) -> f64{
+fn farenheitToCelsius(fDegrees:f64) -> f64{
     (fDegrees -32f64)/1.8f64
+}
+
+
+fn fibonacciHandler(){
+    println!("Introduzca el número de fibonacci que desea obtener:");
+
+    let mut fiboN = String::new();
+
+    io::stdin()
+        .read_line(&mut fiboN)
+        .expect("No se ha podido leer la línea");
+    
+    let fiboN: u64 =  fiboN.trim().parse().expect("Por favor, introduzca un número");
+
+    let fiboNum: u64  = fibo(fiboN);
+    println!("El número de fibonacci en la posición {fiboN} es {fiboNum}");
+
+}
+
+// Terrible implementation, but it's not the point of the program
+fn fibo(n:u64) -> u64{
+    if n > 2{
+        fibo(n-1) + fibo(n-2)
+    } else{
+        1
+    }
 }
