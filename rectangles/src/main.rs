0e228d84868317bcfdc50b71b91ef0fb0dbeaaca
pcardenal5 <pcardenalra@gmail.com>
2024-05-15 11:25:06 +0200

    Area 1

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,10 @@
+fn area(width: u64, height: u64) -> u64 {
+    width * height
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width : u64 = 5;
+    let height : u64 = 10;
+
+    println!("The area of a rectangle of dimensions {}x{} is {}", height, width, area(height, width))
 }
