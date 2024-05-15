bdbdc37d7284d9618ac8e296563b0e9f87ecf2fe
pcardenal5 <pcardenalra@gmail.com>
2024-05-15 11:03:10 +0200

    Implement Display, create new user

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 struct User {
     active :bool,
     username: String,
@@ -5,17 +7,39 @@ struct User {
     sign_in_count: u64
 }
 
+impl fmt::Display for User {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        let mut active_mod = String::new();
+        if !self.active {
+            active_mod = String::from("not ");
+        }
+        write!(f, "The email from the user {} is {}. They are {}active and they logged it {} times.", self.username, self.email, active_mod, self.sign_in_count)
+    }
+}
+
+fn build_user(username: String, email:String) -> User {
+     User {
+        active: true,
+        username,
+        email,
+        sign_in_count : 1u64
+    }
+}
+
+
 fn main() {
-    let mut new_user: User = User {
-        active          : true,
-        username        : String::from("firstUser"),
-        email           : String::from("cool_mail_name@mail.com"),
-        sign_in_count   : 1u64
-    };
-    println!("The email from the user {0} is {1}", new_user.username, new_user.email);
+    let mut new_user: User = build_user(String::from("firstUser"), String::from("cool_mail_name@mail.com"));
+
+    println!("{}", new_user);
     
     new_user.email = String::from("another_cool_mail@coolerdomain.com");
+    new_user.sign_in_count =  new_user.sign_in_count + 1;
 
-    println!("The email from the user {0} has been changed to {1}", new_user.username, new_user.email);
+    println!("{}", new_user);
+    
+    let copy_new_user = User{email : String::from("boring_name@boringdomain.com"), ..new_user};
+    
+    println!("{}", copy_new_user);
+    
 
 }
